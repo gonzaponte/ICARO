f255d525c5f3ced292dbf6660103f4e52f5eda8e
Gonzalo <gonzaponte@gmail.com>
2017-08-24 17:07:47 +0200

    cosmetics and remove redundant functions

--- icaro/Alphas/alphas_lifetime.py
+++ icaro/Alphas/alphas_lifetime.py
@@ -9,8 +9,15 @@ import matplotlib.pyplot as plt
 
 import invisible_cities.core.core_functions as coref
 import invisible_cities.reco. dst_functions as dstf
-import invisible_cities.core.fit_functions  as fitf
+import invisible_cities.core. fit_functions as fitf
 import invisible_cities.database.load_db    as db
+
+from invisible_cities.icaro.hst_functions import plot
+from invisible_cities.icaro.hst_functions import hist
+from invisible_cities.icaro.hst_functions import hist2d
+from invisible_cities.icaro.hst_functions import labels
+from invisible_cities.icaro.hst_functions import plot_writer
+from invisible_cities.icaro.hst_functions import measurement_string
 
 from icaro.core.kdst_functions import event_rate
 from icaro.core.kdst_functions import profile_and_fit
@@ -19,9 +26,13 @@ from icaro.core.kdst_functions import to_deltatime
 from icaro.core.kdst_functions import lifetime_vs_t
 from icaro.core.kdst_functions import save_lifetime
 
-plt.rcParams["figure.figsize"         ] = 10, 8
-plt.rcParams["font.size"              ] = 14
+
+np.warnings.filterwarnings('ignore')
+
+plt.rcParams["figure.figsize"         ] = 16, 12
+plt.rcParams[  "font.size"            ] = 18
 plt.rcParams["figure.max_open_warning"] = 1000
+
 
 space = "\n.\n.\n.\n"
 print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), end=space)
@@ -41,12 +52,12 @@ parser.add_argument("--overwrite" , action="store_true"  , help="overwrite dateb
 
 flags, extras = parser.parse_known_args(args)
 
-flags.i = os.path.expandvars(flags.i if flags.i else "$ALPHASDIR/")
-flags.o = os.path.expandvars(flags.o if flags.o else "$ICARODIR/icaro/Alphas/Lifetimes.txt")
-flags.p = os.path.expandvars(flags.p if flags.p else "./")
+flags.i = os.path.expandvars(flags.i if flags.i else "$DATADIR/")
+flags.o = os.path.expandvars(flags.o if flags.o else "$ICARODIR/icaro/Lifetime/Lifetimes.txt")
+flags.p = os.path.expandvars(flags.p if flags.p else "$ICARODIR/icaro/Lifetime/Plots/")
 
 run_numbers   = flags.r
-run_tags      = flags.t
+run_tags      = flags.t if len(flags.t) == len(flags.r) else flags.t * len(flags.r)
 data_filename = flags.i + "{0}/dst_{0}.root.h5"
 text_filename = flags.o
 run_comment   = flags.c
@@ -54,244 +65,196 @@ plots_folder  = flags.p
 save_plots    = flags.save_plots
 overwrite     = flags.overwrite
 
-if len(run_tags)==1:
-    run_tags *= len (run_numbers)
 
-def savefig(name, run_number):
-    folder = os.path.join(plots_folder, str(run_number))
-    if not save_plots: return
-    if not os.path.exists(plots_folder): os.mkdir(plots_folder)
-    if not os.path.exists(      folder): os.mkdir(      folder)
-    plt.savefig(os.path.join(folder, name + ".png"))
+Xrange        = -200, 200
+Yrange        = -200, 200
+Zrange        =    0, 600
+Xnbins        =   50
+Ynbins        =   50
+Znbins        =   50
+S1nbins       =   50
+S2nbins       =   50
 
+XYrange       = Xrange, Yrange
+XYnbins       = Xnbins, Ynbins
+
+Rfiducial     = 100 # in mm
+Zcathode      = 500 # in µs
 
 for run_number, run_tag in zip(run_numbers, run_tags):
+    run_tag = run_tag.lower()
+    savefig = plot_writer(plots_folder + str(run_number), "png")
+
+    if   run_tag == "alphas":
+        S1range =    0, 3e3
+        S2range =    0, 4e4
+    elif run_tag == "kr":
+        S1range =    0, 5e1
+        S2range =    0, 2e4
+
     full       = dstf.load_dst(data_filename.format(run_number), "DST", "Events")
     t_begin    = np.min(full.time)
     t_end      = np.max(full.time)
     run_dt     = t_end - t_begin
     full.time -= t_begin
-    fid        = full[full.R < 100] # michel sorel cuts
-    cath       = full[full.Z > 500]
-    bulk       = full[full.Z < 500]
+    fid        = full[full.R < Rfiducial] # michel sorel cuts
+    cath       = full[full.Z > Zcathode ]
+    bulk       = full[full.Z < Zcathode ]
 
-    print("Run number            :", run_number)
-    print("# alphas              :", len(full) )
-    print("# alphas at R < 100 mm:", len(fid ) )
-    print("# alphas at Z > 500 µs:", len(cath) )
-    print("# alphas at Z < 500 µs:", len(bulk) )
-
+    print(f"# alphas              :"        , len(full))
+    print(f"# alphas at R < {Rfiducial} mm:", len(fid ))
+    print(f"# alphas at Z > {Zcathode} µs:" , len(cath))
+    print(f"# alphas at Z < {Zcathode} µs:" , len(bulk))
 
     #--------------------------------------------------------
-    plt.figure()
-    n, bins, _ = \
-    plt.hist  (full.time/60, 40)
-    plt.xlabel("Time (min)")
-    plt.ylabel("Rate (({:.1f} min)$^{{-1}}$)".format(np.diff(bins)[0]))
-    plt.title ("Trigger rate")
-    savefig   ("TriggerRate", run_number)
-    
-    rate = event_rate(full)
-    print("Average trigger rate: {:.2f} evts/s".format(rate))
+    _, bins, _ = \
+    hist(full.time/60, Tnbins)
+    labels("Time (min)",
+           "Rate (({:.1f} min)$^{{-1}}$)".format(np.diff(bins)[0]),
+           "Trigger rate")
+    savefig("TriggerRate")
 
+    rate = event_rate(full)
+    print("Average trigger rate: {:.2f} Hz".format(rate))
 
     #--------------------------------------------------------
     plt.figure (figsize=(12, 8))
     plt.subplot(2, 3, 1)
-    plt.hist   (full.S1e, 50, (0, np.max(full.S1e)))
-    plt.xlabel ("S1 energy (pes)")
-    plt.ylabel ("Entries")
-    plt.title  ('S1 energy')
+    plt.hist   (full.S1e, S1nbins, (0, np.max(full.S1e)))
+    labels     ("S1 energy (pes)", "Entries", "S1 energy")
 
     plt.subplot(2, 3, 2)
-    plt.hist   (full.S2e, 50, (0, np.max(full.S2e) * 1.2))
-    plt.xlabel ("S2 energy (pes)")
-    plt.ylabel ("Entries")
-    plt.title  ('S2 energy')
+    plt.hist   (full.S2e, S2nbins, (0, np.max(full.S2e) * 1.2))
+    labels     ("S2 energy (pes)", "Entries", "S2 energy")
 
     plt.subplot(2, 3, 3)
-    plt.hist   (fid.S2e, 50, (0, np.max(full.S2e) * 1.2))
-    plt.xlabel ("S2 energy (pes)")
-    plt.ylabel ("Entries")
-    plt.title  ('S2 energy (R < 100 mm)')
+    plt.hist   (fid .S2e, S2nbins, (0, np.max(full.S2e) * 1.2))
+    labels     ("S2 energy (pes)", "Entries", "S2 energy (R < 100 mm)")
 
     plt.subplot(2, 3, 4)
-    plt.hist   (full.Z, 25, (0, 600))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("Entries")
-    plt.title  ('Drift time')
+    plt.hist   (full.Z  ,  Znbins, Zrange)
+    labels     ("Drift time (µs)", "Entries", "Drift time")
 
     plt.subplot(2, 3, 5)
-    plt.hist   (fid.Z, 25, (500, 600))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("Entries")
-    plt.title  ('Drift time (cathode)')
+    plt.hist   (fid .Z  ,  Znbins, (Zcathode, Zrange[1]))
+    labels     ("Drift time (µs)", "Entries", "Drift time (cathode)")
 
     plt.subplot(2, 3, 6)
-    plt.hist   (fid.Z, 25, (0, 500))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("Entries")
-    plt.title  ('Drift time (bulk)')
+    plt.hist   (fid .Z  ,  Znbins, (Zrange[0], Zcathode))
+    labels     ("Drift time (µs)", "Entries", "Drift time (bulk)")
 
     plt.tight_layout()
-    savefig("EZ_distributions", run_number)
+    savefig("EZ_distributions")
 
 
     #--------------------------------------------------------
-    plt.figure()
-    plt.hist2d(full.time, full.S2e, (25, 25))
-    plt.xlabel("Time (s)")
-    plt.ylabel("Energy (pes)")
-    plt.title ("Energy vs time")
-    savefig   ("EvsT", run_number)
+    hist2d (full.time, full.S2e, (Tnbins, S2nbins))
+    labels ("Time (s)", "Energy (pes)", "Energy vs time")
+    savefig("EvsT")
 
 
     #--------------------------------------------------------
     plt.figure(figsize=(12, 8))
-    zrange  = 0, 610
-    S2range = 0, 4e4
-    S1range = 0, 3e3
 
     plt.subplot(2, 3, 1)
-    plt.hist2d (full.Z, full.S2e, (25, 25), range=(zrange, S2range))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("S2 energy (pes)")
-    plt.title  ('S2 vs Z')
+    plt.hist2d (full.Z  , full.S2e, ( Znbins, S2nbins), range=( Zrange, S2range))
+    labels     ("Drift time (µs)", "S2 energy (pes)", "S2 vs Z")
 
     plt.subplot(2, 3, 2)
-    plt.hist2d (full.Z, full.S1e, (25, 25), range=(zrange, S1range))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("S1 energy (pes)")
-    plt.title  ('S1 vs Z')
+    plt.hist2d (full.Z  , full.S1e, ( Znbins, S1nbins), range=( Zrange, S1range))
+    labels     ("Drift time (µs)", "S1 energy (pes)", "S1 vs Z")
 
     plt.subplot(2, 3, 3)
-    plt.hist2d (full.S2e, full.S1e, (25, 25), range=(S2range, S1range))
-    plt.xlabel ("S2 energy (pes)")
-    plt.ylabel ("S1 energy (pes)")
-    plt.title  ('S1 vs S2')
+    plt.hist2d (full.S2e, full.S1e, (S2nbins, S1nbins), range=(S2range, S1range))
+    labels     ("S2 energy (pes)", "S1 energy (pes)", "S1 vs S2")
 
     plt.subplot(2, 3, 4)
-    plt.hist2d (fid.Z, fid.S2e, (25, 25), range=(zrange, S2range))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("S2 energy (pes)")
-    plt.title  ('S2 vs Z (R < 100 mm)')
+    plt.hist2d (fid .Z  , fid .S2e, ( Znbins, S2nbins), range=( Zrange, S2range))
+    labels     ("Drift time (µs)", "S2 energy (pes)", "S2 vs Z (R < 100 mm)")
 
     plt.subplot(2, 3, 5)
-    plt.hist2d (fid.Z, fid.S1e, (25, 25), range=(zrange, S1range))
-    plt.xlabel ("Drift time ($\mu$s)")
-    plt.ylabel ("S1 energy (pes)")
-    plt.title  ('S1 vs Z (R < 100 mm)')
+    plt.hist2d (fid .Z  , fid .S1e, ( Znbins, S1nbins), range=( Zrange, S1range))
+    labels     ("Drift time (µs)", "S1 energy (pes)", "S1 vs Z (R < 100 mm)")
 
     plt.subplot(2, 3, 6)
-    plt.hist2d (fid.S2e, fid.S1e, (25, 25), range=(S2range, S1range))
-    plt.xlabel ("S2 energy (pes)")
-    plt.ylabel ("S1 energy (pes)")
-    plt.title  ('S1 vs S2 (R < 100 mm)')
+    plt.hist2d (fid .S2e, fid .S1e, (S2nbins, S1nbins), range=(S2range, S1range))
+    labels     ("S2 energy (pes)", "S1 energy (pes)", "S1 vs S2 (R < 100 mm)")
 
     plt.tight_layout()
-    savefig("S12", run_number)
+    savefig("S12")
 
 
     #--------------------------------------------------------
-    xrange  = -215, 215
-    yrange  = -215, 215
-    xyrange = xrange, yrange
-    xybins  = 50, 50
-
     plt.figure (figsize=(12,10))
     plt.subplot(2, 2, 1)
-    plt.hist2d (full.X, full.Y, xybins, xyrange)
-    plt.xlabel ("X (mm)")
-    plt.ylabel ("Y (mm)")
-    plt.title  ('XY distribution')
+    plt.hist2d (full.X, full.Y, XYnbins, XYrange)
+    labels     ("X (mm)", "Y (mm)", "XY distribution")
 
     plt.subplot(2, 2, 2)
-    plt.hist2d (fid.X, fid.Y, xybins, xyrange)
-    plt.xlabel ("X (mm)")
-    plt.ylabel ("Y (mm)")
-    plt.title  ('XY distribution (R < 100 mm)')
+    plt.hist2d (fid .X, fid .Y, XYnbins, XYrange)
+    labels     ("X (mm)", "Y (mm)", "XY distribution (R < 100 mm)")
 
     plt.subplot(2, 2, 3)
-    plt.hist2d (bulk.X, bulk.Y, xybins, xyrange)
-    plt.xlabel ("X (mm)")
-    plt.ylabel ("Y (mm)")
-    plt.title  ('XY distribution (bulk)')
+    plt.hist2d (bulk.X, bulk.Y, XYnbins, XYrange)
+    labels     ("X (mm)", "Y (mm)", "XY distribution (bulk)")
 
     plt.subplot(2, 2, 4)
-    plt.hist2d (cath.X, cath.Y, xybins, xyrange)
-    plt.xlabel ("X (mm)")
-    plt.ylabel ("Y (mm)")
-    plt.title  ('XY distribution (cathode)')
+    plt.hist2d (cath.X, cath.Y, XYnbins, XYrange)
+    labels     ("X (mm)", "Y (mm)", "XY distribution (cathode)")
 
     plt.tight_layout()
-    savefig("XY", run_number)
+    savefig("XY")
 
-    
+
     #--------------------------------------------------------
-    zrange =  50, 400
-    Erange = 1e4, 7e4
-    nbins  =  50
-    seed   = Erange[1], zrange[1]/np.log(Erange[0]/Erange[1])
+    seed   = S2range[1], -1e3
 
     plt.figure()
     F, x, y, sy = profile_and_fit(fid.Z, fid.S2e, 
-                                  xrange = zrange, 
-                                  yrange = Erange, 
-                                  nbins  = nbins, 
-                                  fitpar = seed,
-                                  label  = ("Drift time ($\mu$s)", "S2 energy (pes)"))
-    savefig("LifetimeFit", run_number)
+                                  xrange =  Zrange,
+                                  yrange = S2range,
+                                  nbins  =  Znbins,
+                                  fitpar =  seed)
+    labels ("Drift time (µs)", "S2 energy (pes)")
+    savefig("LifetimeFit")
 
-    print("Energy at Z=0: {:.0f} +- {:.0f} pes".format( F.values[0], F.errors[0]))
-    print("Lifetime     : {:.1f} +- {:.1f} µs ".format(-F.values[1], F.errors[1]))
-    print("Chi2 fit     : {:.2f}              ".format( F.chi2))
-    LT, LTu = -F.values[1], F.errors[1]
-    E0, E0u =  F.values[0], F.errors[0]
+    E0, u_E0 =  F.values[0], F.errors[0]
+    LT, u_LT = -F.values[1], F.errors[1]
 
-    
+    print("Energy at Z=0: ({}) pes".format(measurement_string(E0, u_E0)))
+    print("Lifetime     : ({}) µs ".format(measurement_string(LT, u_LT)))
+    print("Chi2 fit     : {:.2f}  ".format(F.chi2))
+
     #--------------------------------------------------------
     plt.figure()
-    dst        = fid[coref.in_range(fid.Z, *zrange)]
+    dst        = fid[coref.in_range(fid.Z, *Zrange)]
     timestamps = list(map(time_from_timestamp, dst.time))
 
-    lifetime_vs_t(dst, nslices=8, timestamps=timestamps)
-    plt.xlabel   ("Time (s)")
-    plt.ylabel   ("Lifetime (µs)")
-    plt.title    ("Lifetime evolution within run");
-    savefig      ("LifetimeT", run_number)
+    lifetime_vs_t(dst, nslices=8, timestamps=timestamrps)
+    labels       ("Time (s)", "Lifetime (µs)", "Lifetime evolution within run")
+    savefig      ("LifetimeT")
 
 
     #--------------------------------------------------------    
-    def get_centers(xbins):
-        # xbins should be shifted, as it contains the lower bounds
-        # and we want bin centers
-        return xbins[:-1] + np.diff(xbins) * 0.5
+    values, bins = np.histogram(cath.Z, 50)
 
-    plt.figure()
+    mean_val = bins[np.argmax(values)]
+    zrange_  = mean_val - 5., mean_val + 5.
 
-    values, bins = np.histogram(cath.Z,50)
-    mean_val = get_centers(bins)[np.argmax(values)]
+    y, x, _  = hist(cath.Z, Znbins, zrange_)
+    F        = fitf.fit(fitf.gauss, x, y, (np.max(values), mean_val, 2))
 
-    zrange   =  mean_val-5, mean_val+5
-    nbins    =  50
-    y, x, _  = plt.hist(cath.Z,nbins, zrange)
-    x        = get_centers(x)
+    plot   (x, F.fn(x), lw=3, c='r')
+    labels ("Drift length (µs)", "Entries", "Max drift length")
+    savefig("MaxDriftLength")
 
-    F        = fitf.fit(fitf.gauss, x, y, (500, mean_val, 2))
+    max_drift   = F.values[1]
+    u_max_drift = F.errors[1]
+    v_drift     = db.DetectorGeo().ZMAX[0]/max_drift
+    u_v_drift   = v_drift*u_max_drift/max_drift
 
-    v_drift  = db.DetectorGeo().ZMAX[0]/F.values[1]
-    ev_drift = v_drift*F.errors[1]/F.values[1]
-
-    print("Drift length   : {:.3f} +- {:.3f} pes".format( F.values[1], F.errors[1]))
-    print("Drift velocity : {:.7f} +- {:.1e} µs ".format( v_drift    , ev_drift))
-
-    #--------------------------------------------------------
-
-    plt.plot     (x, F.fn(x), lw=3, c='r')
-    plt.xlabel   ('Drift length ($\mu$s)')
-    plt.ylabel   ('Entries')
-    plt.title    ('Drift length')
-    savefig      ("Drift:length", run_number)
+    print("Drift time     : {} µs   ".format(measurement_string(max_drift, u_max_drift)))
+    print("Drift velocity : {} mm/µs".format(measurement_string(  v_drift,   u_v_drift)))
 
 
     #--------------------------------------------------------
@@ -299,10 +262,11 @@ for run_number, run_tag in zip(run_numbers, run_tags):
     date_end   = time_from_timestamp(t_end  )
     date_lapse = to_deltatime       (t_begin, t_end, unit="s", to_str=True)
 
-    save_lifetime(text_filename, run_number,    run_tag,         
-                             LT,        LTu,
-                             E0,        E0u,
-                        v_drift,   ev_drift,
+    save_lifetime(text_filename,
+                     run_number,    run_tag,
+                             LT,       u_LT,
+                             E0,       u_E0,
+                        v_drift,  u_v_drift,
                         t_begin,      t_end,     run_dt,
                      date_begin,   date_end, date_lapse,
                      comment   = run_comment.replace(" ", "_"),
--- icaro/core/kdst_functions.py
+++ icaro/core/kdst_functions.py
@@ -1,14 +1,15 @@
-import sys
-import numpy as np
-import tables as tb
-import pandas as pd
-import invisible_cities.core.fit_functions as fitf
-from   invisible_cities.core.system_of_units_c import units
-from   invisible_cities.icaro.mpl_functions import set_plot_labels
-from   invisible_cities.core.core_functions import in_range
-import matplotlib.pyplot as plt
-from   collections import namedtuple
 import datetime
+
+import numpy  as np
+import pandas as pd
+
+import invisible_cities.core.fit_functions  as  fitf
+import invisible_cities.core.core_functions as coref
+
+from invisible_cities.evm  .ic_containers import Measurement
+from invisible_cities.icaro.hst_functions import plot
+from invisible_cities.icaro.hst_functions import errorbar
+from invisible_cities.icaro.hst_functions import labels
 
 
 def delete_lifetime_entry(filename, run_number, delimiter=" ", overwrite=False):
@@ -23,7 +24,8 @@ def delete_lifetime_entry(filename, run_number, delimiter=" ", overwrite=False):
     return False
 
 
-def save_lifetime(    filename, run_number, run_tag,
+def save_lifetime(    filename,
+                    run_number,    run_tag,
                             lt,       u_lt,
                            E_0,       u_E0,
                        v_drift,  u_v_drift,
@@ -91,29 +93,35 @@ def lifetime(dst, zrange=(25,530), Erange=(1e+3, 70e3), nbins=10):
     TSTAMP  = []
 
     for i in range(nbins):
-        k0 = i * indx
-        k = (i+1) * indx - 1
-        print(' ---fit over events between {} and {}'.format(k0, k))
+        k0 =  i    * indx
+        k1 = (i+1) * indx - 1
+
+        print(' ---fit over events between {} and {}'.format(k0, k1))
         st0 = time_from_timestamp(dst.time.values[k0])
-        st =  time_from_timestamp(dst.time.values[k])
+        st =  time_from_timestamp(dst.time.values[k1])
 
-        print('time0 = {} time1 = {}'.format(st0,st))
+        print('time0 = {} time1 = {}'.format(st0, st))
 
-        tleg = dst[in_range(dst.time.values, minval=dst.time.values[k0], maxval=dst.time.values[k])]
+        tleg = dst[coref.in_range(dst.time.values,
+                                  minval = dst.time.values[k0],
+                                  maxval = dst.time.values[k1])]
+
         print('size of time leg = {}'.format(len(tleg)))
-        F, x, y, sy = profile_and_fit(tleg.Z, tleg.S2e,
-                                      xrange=zrange,
-                                      yrange=Erange,
-                                      nbins=nbins,
-                                      fitpar=(50000,-300),
-                                      label=("Drift time ($\mu$s)", "S2 energy (pes)"))
+        F, x, y, u_x, u_y = profile_and_fit(tleg.Z, tleg.S2e,
+                                            xrange = zrange,
+                                            yrange = Erange,
+                                            nbins  = nbins,
+                                            fitpar = (50000, -300))
+        labels("Drift time (µs)", "S2 energy (pes)", "Lifetime fit")
         print_fit(F)
-        chi = chi2(F, x, y, sy)
+
+        chi = chi2(F, x, y, u_y)
         print('chi2 = {}'.format(chi))
-        CHI2.append(chi)
-        LAMBDA.append(F.values[1])
+
+        CHI2   .append(chi)
+        LAMBDA .append(F.values[1])
         ELAMBDA.append(F.errors[1])
-        TSTAMP.append(st)
+        TSTAMP .append(st)
 
     TIME = [datetime.datetime.strptime(elem,
            '%Y-%m-%d %H:%M:%S') for elem in TSTAMP]
@@ -130,41 +138,20 @@ def lifetime_vs_t(dst, nslices=10, nbins=10, seed=(3e4, -5e2), timestamps=None, 
     tmax = np.max(dst.time)
     bins = np.linspace(tmin, tmax, nslices+1)
     for t0, t1 in zip(bins[:-1], bins[1:]):
-        subdst   = dst[in_range(dst.time, t0, t1)]
+        subdst   = dst[coref.in_range(dst.time, t0, t1)]
         Z, E, Eu = fitf.profileX(subdst.Z, subdst.S2e, nbins, **profOpt)
         f        = fitf.fit(fitf.expo, Z, E, seed, sigma=Eu)
 
         LT .append(-f.values[1] )
         LTu.append( f.errors[1] )
-        E0 .append(f.values[0] )
+        E0 .append( f.values[0] )
         E0u.append( f.errors[0] )
         T  .append(0.5*(t1 + t0))
         Tu .append(0.5*(t1 - t0))
 
-    plt.errorbar(T, LT, LTu, Tu)
-
-    #plt.errorbar(T, E0, E0u, Tu)
+    errorbar(T, LT, LTu, Tu)
+    errorbar(T, E0, E0u, Tu)
     return T, LT, Tu, LTu, E0, E0u
-
-class MapXY:
-    def __init__(self, x, y, E):
-        self.xs   = x.reshape(x.size, 1) #file to column vector
-        self.ys   = y.reshape(y.size, 1)
-        self.eref = E[E.shape[0]//2, E.shape[1]//2]
-        self.es   = E
-        print('reference energy = {}'.format(self.eref))
-
-    def xycorr(self, x, y):
-        x_closest  = np.apply_along_axis(np.argmin, 0, abs(x - self.xs))
-        y_closest  = np.apply_along_axis(np.argmin, 0, abs(y - self.ys))
-        e          = self.es[x_closest, y_closest]
-        e[e < 1e3] = self.eref
-        return self.eref / e
-
-
-def load_dst(filename):
-    with tb.open_file(filename) as h5:
-        return pd.DataFrame.from_records(h5.root.DST.Events.read())
 
 
 def event_rate(kdst):
@@ -173,59 +160,37 @@ def event_rate(kdst):
     return kdst.event.size/(t1-t0)
 
 
-def profile_and_fit(X, Y, xrange, yrange, nbins, fitpar, label, fitOpt  = "r"):
-    n_it = 0
-    chi2 = 0
-    #while not 0.8 < chi2 < 1.4:
-    xe       = 0.5*(xrange[1] - xrange[0])/nbins
-    x, y, sy = fitf.profileX(X, Y, nbins=nbins, xrange=xrange, yrange=yrange)
-    sel      = fitf.in_range(x, *xrange)
-    x, y, sy = x[sel], y[sel], sy[sel]
-    f        = fitf.fit(fitf.expo, x, y, fitpar, sigma=sy)
-    chi2     = f.chi2
-    nbins   -= n_it
+def profile_and_fit(X, Y, xrange, yrange, nbins, fitpar, fitOpt  = "r"):
+#    n_it = 0
+#    chi2 = 0
+#    while not 0.8 < chi2 < 1.4:
+    u_x       = 0.5*(xrange[1] - xrange[0])/nbins
+    x, y, u_y = fitf .profileX(X, Y, nbins=nbins, xrange=xrange, yrange=yrange)
+    f         = fitf.fit(fitf.expo, x, y, fitpar, sigma=u_y)
+#        chi2      = f.chi2
+#        nbins    -= n_it
 
-     #   n_it += 1
-      #  if nbins < 5:
-       #     print("Chi2 does not get close to 1")
-        #    break
+#        n_it += 1
+#        if nbins < 5:
+#            print("Chi2 does not get close to 1")
+#            break
 
-    plt.errorbar(x=x, xerr=xe, y=y, yerr=sy,
-                 linestyle='none', marker='.')
-    plt.plot(x, f.fn(x), fitOpt)
-    set_plot_labels(xlabel=label[0], ylabel=label[1], grid=True)
-    return f, x, y, sy
-
-
-def profile_and_fit_radial(X, Y, xrange, yrange, nbins, fitpar, label, fitOpt="r"):
-    xe       = (xrange[1] - xrange[0])/nbins
-    x, y, sy = fitf.profileX(X, Y, nbins=nbins, xrange=xrange, yrange=yrange)
-    sel      = fitf.in_range(x, *xrange)
-    x, y, sy = x[sel], y[sel], sy[sel]
-    f        = fitf.fit(fitf.polynom, x, y, fitpar, sigma=sy)
-
-    plt.errorbar(x=x, xerr=xe, y=y, yerr=sy,
-                 linestyle='none', marker='.')
-    plt.plot(x, f.fn(x), fitOpt)
-    set_plot_labels(xlabel=label[0], ylabel=label[1], grid=True)
-    return f, x, y, sy
+    errorbar(x, y, u_y, u_x,
+             linestyle = "none",
+             marker    = ".")
+    plot    (x, f.fn(x),
+             fitOpt,
+             new_figure=False)
+    return f, x, y, u_x, u_y
 
 
 def chi2(F, X, Y, SY):
-    fitx = F.fn(X)
     n = len(F.values)
     print('degrees of freedom = {}'.format(n))
-    chi2t = 0
-    for i, x in enumerate(X):
-        chi2 = abs(Y[i] - fitx[i])/SY[i]
-        chi2t += chi2
-        #print('x = {} f(x) = {} y = {} ey = {} chi2 = {}'.format(
-               #x, fitx[i], Y[i], SY[i], chi2 ))
-    return chi2t/(len(X)-n)
 
-    #chi2 = np.sum(np.ma.masked_invalid((fitx - y)**2/sy**2))
-    #print('chi2 = {}'.format(chi2))
+    return np.abs(Y - F.fn(X))/SY/(len(X) - n)
+
 
 def print_fit(f):
-    for i, val in enumerate(f.values):
-        print('fit par[{}] = {} error = {}'.format(i, val, f.errors[i]))
+    for i, val in enumerate(map(Measurement, f.values, f.errors)):
+        print("Parameter {}: {}".format(i, val))
